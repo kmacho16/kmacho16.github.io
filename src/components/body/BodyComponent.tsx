cb26912437ec9175e870ae2488abf6e2a2410b4e
Juan Camacho <juancamacho@seven4n.com>
2021-04-20 21:29:18 -0500

    Add cv section

--- src/components/body/BodyComponent.tsx
+++ src/components/body/BodyComponent.tsx
@@ -1,6 +1,18 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, useState } from 'react';
+import hvEn from './resource/HV-EN.pdf';
+import hvEs from './resource/HV-ES.pdf';
+
 import './BodyComponent.css';
+
 const BodyComponent = () => {
+  const [lang, setLan] = useState<string>('EN');
+  const [btnText, setBtnText] = useState<string>('Get my CV');
+  const downloadPdf = () => {
+    var link = document.createElement('a');
+    link.href = lang == 'ES' ? hvEs : hvEn;
+    link.download = `JuanCamacho-${lang}.pdf`;
+    link.dispatchEvent(new MouseEvent('click'));
+  };
   return (
     <div className="about">
       <h1 className="red-text-opaque uppercase">About me</h1>
@@ -10,13 +22,36 @@ const BodyComponent = () => {
           Caldas de Caldas, Software developer with experience in solution
           problems quickly and effectively. With constant curiosity and craving
           for continuous learning, aware that work gives best results with
-          environments friendly and communicative.
+          environments friendly and communicative.
         </p>
         <p>
           I believe that being immersed in a technological field, the
           skillslearning abilities and self-determination are the key to a good
           professional development and growth
         </p>
+      </div>
+      <button className="get-cv-btn" onClick={downloadPdf}>
+        {btnText} <span>&#128516;</span>
+      </button>
+      <div className="content-switch">
+        <span className={lang == 'ES' ? 'lan-active' : 'lan'}>ES</span>
+        <label className="switch">
+          <input
+            defaultChecked={true}
+            type="checkbox"
+            onChange={e => {
+              if (e.target.checked) {
+                setLan('EN');
+                setBtnText('GET MY CV');
+              } else {
+                setLan('ES');
+                setBtnText('Aquí está mi HV');
+              }
+            }}
+          />
+          <span className="slider round"></span>
+        </label>
+        <span className={lang == 'EN' ? 'lan-active' : 'lan'}>EN</span>
       </div>
     </div>
   );
